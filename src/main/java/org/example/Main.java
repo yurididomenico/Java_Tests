6e3ddad4603709ca64f84a2bb741c8f440b5ad90
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-12-27 12:12:19 +0100

    Variables: declaration & assignment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main program develop branch\n");
-        int x; // Declaration
-        x = 10; // Assignment
+        int x = 10; // Declaration & Assignment
         System.out.println(x);
     }
 }
