4b70404003c3ba655e331f93264772153c3d17e6
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-12-27 12:25:47 +0100

    Types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main program develop branch\n");
-        int x = 10; // Declaration & Assignment
-        System.out.println(x);
+
+        // Declaration, Assignment, Initialization
+        int x; // Declaration
+        x = 10; // Assignment
+        System.out.println("x: " + x);
+        int y = 20; // Initialization
+        System.out.println("y: " + y + "\n");
+
+        // Types
+        int i = 1; // Int
+        float f = 2.3f; // Float
+        String s = "Ciao!"; // String
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(s);
     }
 }
